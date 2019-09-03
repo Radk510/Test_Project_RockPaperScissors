a9274b9ee2e3fc806f6f9d5a63f6e69a9660a134
radk510 <radk510@yandex.ru>
2019-09-03 15:02:05 +0300

    feat: start to model game process

---
+++ Test_Project_RockPaperScissors/GameProcess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Project_RockPaperScissors
+{
+    class GameProcess
+    {
+        
+        public void GameIntroduction()
+        {
+            Console.WriteLine("This is a popular game in console view called \"Rock, Paper & Scissors\". " +
+                "Created by Rodion Kl");
+        }
+
+        
+
+    }
+}
